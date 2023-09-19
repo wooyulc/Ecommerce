d289e673fc8d77e247295ce970f8c44e7f24630a
Wooyul Chung <ttmac@Wooyuls-MacBook-Air.local>
2023-09-20 05:06:19 +0800

    Product detail page

--- fronted/src/screen/ProductScreen.jsx
+++ fronted/src/screen/ProductScreen.jsx
@@ -9,6 +9,57 @@ const ProductScreen = () => {
     const product = products.find((p) => p._id === productId);
 
     return <>
+        <Link to='/'>Go back</Link>
+        <Row>
+            <Col md={5}>
+                <Image src={product.image} alt={product.name} fluid />
+            </Col>
+            <Col md={4}>
+                <ListGroup.Item>
+                    <h3>{product.name}</h3>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    <Rating value={product.rating} text={`${product.numReviews} review`} />
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    Price: ${product.price}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    Description: ${product.description}
+                </ListGroup.Item>
+            </Col>
+            <Col md={3}>
+                <Card>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <Row>
+                                <Col>Price:</Col>
+                                <Col>
+                                    <strong>${product.price}</strong>
+                                </Col>
+                            </Row>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Row>
+                                <Col>Status:</Col>
+                                <Col>
+                                    <strong>{product.countInStock > 0 ? 'In Stock' : 'Out of stock'}</strong>
+                                </Col>
+                            </Row>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Button 
+                                type='button'
+                                disabled={product.countInStock === 0}
+                            >
+                                Add to Cart
+                            </Button>
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Card>
+            </Col>
+
+        </Row>
     </>
 }
 
