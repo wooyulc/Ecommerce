951f9494e6c48750ba635fc92a79c03cd180ebc9
Wooyul Chung <wooyulc@uci.edu>
2024-01-28 16:48:37 +0900

    server & fetching data

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
+import { useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom'
-import products from '../products'
+import axios from 'axios'
 import { Link } from 'react-router-dom'
 import Rating from '../components/Rating'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 
 const ProductScreen = () => {
-  const { id: productId } = useParams();
-  const product = products.find((p)=>p._id === productId);
+  const [product, setProduct] = useState([]);
+  const {id: productId} = useParams();
+  
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const {data} = await axios.get(`/api/products/${productId}`);
+      setProduct(data);
+    };
+    fetchProduct();
+  },[productId]);
+
+  
 
   return (
     <>
